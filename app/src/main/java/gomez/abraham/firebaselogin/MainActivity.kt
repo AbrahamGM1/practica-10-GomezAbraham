f0683e0d1e9581791e4bbd595d689feff235ad67
ABRAHAM <abraham.gomez228796@potros.itson.edu.mx>
2023-04-23 18:48:24 -0700

    Práctica 10 realizada

---
+++ app/src/main/java/gomez/abraham/firebaselogin/MainActivity.kt
@@ -0,0 +1,25 @@
+package gomez.abraham.firebaselogin
+
+import android.os.Binder
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import gomez.abraham.firebaselogin.R
+import gomez.abraham.firebaselogin.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        auth = Firebase.auth
+    }
+}
